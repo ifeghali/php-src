3906344122c33103ee10a679d7e4f0f4cf033f27
zeev <zeev>
1999-05-09 08:48:05 +0000

    Finalizing the PHP version of SAPI. Support POST and cookies among other     things. Fully implement ISAPI support - POST and cookies among other things. Almost completely rewrote phpinfo(). Allow modules to easily display their     information in phpinfo() without modifying phpinfo() itself (prototype     for the module info function was changed, thus the large amount of     updated module files). Initial extended SAPI support for Apache, completely untested. CGI now uses SAPI fully as well.

--- ext/oracle/oracle.h
+++ ext/oracle/oracle.h
@@ -1,4 +1,4 @@
-/* $Id: oracle.h,v 1.37 1999/04/30 09:49:23 thies Exp $ */
+/* $Id: oracle.h,v 1.2 1999/04/30 18:59:14 thies Exp $ */
 
 #ifndef _PHP3_ORACLE_H
 #define _PHP3_ORACLE_H
@@ -136,7 +136,7 @@ extern void php3_Ora_Rollback(INTERNAL_FUNCTION_PARAMETERS);
 extern int php3_minit_oracle(INIT_FUNC_ARGS);
 extern int php3_mshutdown_oracle(SHUTDOWN_FUNC_ARGS);
 extern int php3_rshutdown_oracle(SHUTDOWN_FUNC_ARGS);
-extern void php3_info_oracle(void);
+extern void php3_info_oracle(ZEND_MODULE_INFO_FUNC_ARGS);
 extern int php3_rinit_oracle(INIT_FUNC_ARGS);
 
 #else
