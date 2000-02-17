983c832fb2e673c00d7f74afc05885c80298476a
zeev <zeev>
2000-02-17 20:25:08 +0000

    No need for a value (not that it seems to work anyway...)

--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -17,9 +17,9 @@
    |          David Sklar <sklar@student.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_apache.c,v 1.3 2000/02/10 20:24:28 zeev Exp $ */
+/* $Id: php_apache.c,v 1.4 2000/02/16 17:46:14 zeev Exp $ */
 
-#define _PHP_REGEX_H 1
+#define _PHP_REGEX_H
 
 #include "php.h"
 #include "ext/standard/head.h"
