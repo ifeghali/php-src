a4242819bbcdf3464fc4ebaf37dd7c88779f14e9
zeev <zeev>
2000-05-18 15:34:45 +0000

    Update the license with the new clause 6

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -4,10 +4,10 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.01 of the PHP license,      |
+   | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
    | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_01.txt.                                 |
+   | http://www.php.net/license/2_02.txt.                                 |
    | If you did not receive a copy of the PHP license and are unable to   |
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
@@ -23,7 +23,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.24 2000/05/11 13:52:24 andi Exp $ */
+/* $Id: php_imap.c,v 1.25 2000/05/17 17:54:54 hholzgra Exp $ */
 
 #define IMAP41
 
--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -4,10 +4,10 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.01 of the PHP license,      |
+   | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
    | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_01.txt.                                 |
+   | http://www.php.net/license/2_02.txt.                                 |
    | If you did not receive a copy of the PHP license and are unable to   |
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
@@ -17,7 +17,7 @@
    |          David Sklar <sklar@student.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_apache.c,v 1.13 2000/04/07 17:09:18 cmv Exp $ */
+/* $Id: php_apache.c,v 1.14 2000/04/30 04:15:26 shane Exp $ */
 
 #define NO_REGEX_EXTRA_H
 
