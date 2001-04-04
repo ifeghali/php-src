a35c1d72ef8e48c955cc43cc5182cd757bf8d648
fmk <fmk>
2001-04-04 20:10:14 +0000

    Adding empty RSHUTDOWN function.

--- ext/fbsql/php_fbsql.h
+++ ext/fbsql/php_fbsql.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: php_fbsql.h,v 1.6 2001/03/13 20:53:38 frank Exp $ */
+/* $Id: php_fbsql.h,v 1.1 2001/03/13 20:58:47 fmk Exp $ */
 
 #define HAVE_FBSQL 1
 
@@ -39,8 +39,9 @@ extern zend_module_entry fbsql_module_entry;
 #include <FBCAccess/FBCAccess.h>
 
 extern PHP_MINIT_FUNCTION(fbsql);
-extern PHP_RINIT_FUNCTION(fbsql);
 extern PHP_MSHUTDOWN_FUNCTION(fbsql);
+extern PHP_RINIT_FUNCTION(fbsql);
+extern PHP_RSHUTDOWN_FUNCTION(fbsql);
 PHP_MINFO_FUNCTION(fbsql);
 PHP_FUNCTION(fbsql_connect);
 PHP_FUNCTION(fbsql_pconnect);
@@ -106,7 +107,7 @@ ZEND_BEGIN_MODULE_GLOBALS(fbsql)
    long databaseCount;
    long resultCount;
 
-   int linkIndex;
+   unsigned int linkIndex;
    int databaseIndex;
    int resultIndex;
 ZEND_END_MODULE_GLOBALS(fbsql)
