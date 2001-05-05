81eb99fbf07d1add995c566e1dd9e3af00bba314
sas <sas>
2001-05-05 15:33:49 +0000

    Fix libtool check Submitted by: brisse@ping.dk

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -70,7 +70,7 @@ fi
 lt_version=`echo $lt_pversion|sed -e 's/\([a-z]*\)$/.\1/'`
 IFS=.; set $lt_version; IFS=' '
 
-if test "$1$2$3" -ge "133" || (test "$1" = "1" && test "$2" -ge "4")
+if test "$1" -gt "1" || test "$2" -gt "3" || (test "$2" = "3" && (test "$3" = "c" || test "$3" -ge "3"))
 then
 echo "buildconf: libtool version $lt_pversion (ok)"
 else
