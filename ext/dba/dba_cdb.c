858cfabc6d752b0048f42518242cf63aa02412ea
rasmus <rasmus>
2001-06-05 13:12:07 +0000

    vim-6 does folding - clean up a bunch of missing folding tags plus some     misguided RINIT and RSHUTDOWN calls in a few fringe extensions

--- ext/dba/dba_cdb.c
+++ ext/dba/dba_cdb.c
@@ -27,7 +27,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: dba_cdb.c,v 1.5 2000/07/10 10:09:14 sas Exp $ */
+/* $Id: dba_cdb.c,v 1.6 2001/04/30 12:43:32 andi Exp $ */
 
 #include "php.h"
 
@@ -218,3 +218,11 @@ DBA_SYNC_FUNC(cdb)
 }
 
 #endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim: sw=4 ts=4 tw=78 fdm=marker
+ */
