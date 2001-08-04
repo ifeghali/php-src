708cdc674a14c129c27d5f4ca3d1f9fb2b668a98
sebastian <sebastian>
2001-08-04 17:07:16 +0000

    Another one bites the dust.

--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -17,7 +17,7 @@
    |          David Sklar <sklar@student.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_apache.c,v 1.38 2001/07/28 14:01:24 rasmus Exp $ */
+/* $Id: php_apache.c,v 1.39 2001/07/30 06:18:11 zeev Exp $ */
 
 #define NO_REGEX_EXTRA_H
 
@@ -342,7 +342,6 @@ PHP_FUNCTION(getallheaders)
     array_header *env_arr;
     table_entry *tenv;
     int i;
-    TSRMLS_FETCH();
     TSRMLS_FETCH();
 	
     if (array_init(return_value) == FAILURE) {
