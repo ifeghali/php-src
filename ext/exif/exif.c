afeea135f665c31783addb580236b7a606ac13bb
sebastian <sebastian>
2002-02-28 06:26:50 +0000

    Fix headers.

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -1,8 +1,8 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP version 4.0                                                      |
+   | PHP Version 4                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2001 The PHP Group                                |
+   | Copyright (c) 1997-2002 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: $ */
+/* $Id: exif.c,v 1.37 2002/02/28 01:16:26 helly Exp $ */
 
 /* Much of the code in this module was borrowed from the public domain
    jhead.c package with the author's consent.  The main changes have been
