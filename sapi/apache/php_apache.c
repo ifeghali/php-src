4b1cc603a66009f6f3c3b2e5b63924b1b359b2f8
rasmus <rasmus>
2002-08-13 04:03:20 +0000

    Fix proto

--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -17,7 +17,7 @@
    |          David Sklar <sklar@student.net>                             |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_apache.c,v 1.64 2002/06/01 10:27:51 andi Exp $ */
+/* $Id: php_apache.c,v 1.65 2002/06/01 17:38:23 sas Exp $ */
 
 #include "php_apache_http.h"
 
@@ -108,7 +108,7 @@ zend_module_entry apache_module_entry = {
 	STANDARD_MODULE_PROPERTIES
 };
 
-/* {{{ proto bool string apache_child_terminate(void)
+/* {{{ proto bool apache_child_terminate(void)
    Terminate apache process after this request */
 PHP_FUNCTION(apache_child_terminate)
 {
