cd702e6d01f2b2cecb40a78e6cbc5262a5e0f5ca
fmk <fmk>
2002-11-26 07:04:31 +0000

    Avoid , at the end of each string (to, cc and bcc)

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.142 2002/10/09 09:20:46 mfischer Exp $ */
+/* $Id: php_imap.c,v 1.143 2002/11/25 12:30:23 hholzgra Exp $ */
 
 #define IMAP41
 
@@ -3162,7 +3162,7 @@ int _php_imap_mail(char *to, char *subject, char *message, char *headers, char *
 			addr = addr->next;
 		}
 		efree(tempMailTo);
-		bufferTo[offset] = 0;
+		if (offset>0) bufferTo[offset-1] = 0;
 	}
 
 	if (cc && *cc) {
@@ -3180,7 +3180,7 @@ int _php_imap_mail(char *to, char *subject, char *message, char *headers, char *
 			addr = addr->next;
 		}
 		efree(tempMailTo);
-		bufferCc[offset] = 0;
+		if (offset>0) bufferCc[offset-1] = 0;
 	}
 
 	if (bcc && *bcc) {
@@ -3198,7 +3198,7 @@ int _php_imap_mail(char *to, char *subject, char *message, char *headers, char *
 			addr = addr->next;
 		}
 		efree(tempMailTo);
-		bufferBcc[offset] = 0;
+		if (offset>0) bufferBcc[offset-1] = 0;
 	}
 
 
