b83213e52e923714a6e9f2f491fa93ec969eb75f
sniper <sniper>
2003-01-23 16:29:49 +0000

    Fix bug: #21817 (only enable this if --with-kerberos is used)

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.153 2003/01/20 19:41:58 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.154 2003/01/20 21:28:09 iliaa Exp $ */
 
 #define IMAP41
 
@@ -418,7 +418,7 @@ PHP_MINIT_FUNCTION(imap)
 #ifndef PHP_WIN32
 	auth_link(&auth_log);		/* link in the log authenticator */
 	auth_link(&auth_md5);       /* link in the cram-md5 authenticator */ 
-#ifdef HAVE_IMAP_AUTH_GSS
+#ifdef HAVE_IMAP_KRB && defined(HAVE_IMAP_AUTH_GSS)
 	auth_link(&auth_gss);		/* link in the gss authenticator */
 #endif
 
