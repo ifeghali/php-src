2480e35715bccf9e3f9753c1d7e15b652988b249
sniper <sniper>
2003-07-20 10:53:22 +0000

    Missing headers

--- ext/sockets/win32_socket_constants.h
+++ ext/sockets/win32_socket_constants.h
@@ -1,4 +1,22 @@
-/* $Id: snmp.c,v 1.84 2003/07/16 09:28:47 sniper Exp $ */
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2003 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Jason Greene <jason@php.net>                                 |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: win32_socket_constants.h,v 1.2 2003/07/19 18:32:04 andrey Exp $ */
 
 /* This file is to be included by sockets.c */
 
