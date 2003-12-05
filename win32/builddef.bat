4d392eb779ab0215194fff07169fda04d84737a2
fmk <fmk>
2003-12-05 19:51:52 +0000

    Move the echo off statement to the top of the file. This removes commens     from the final def file

--- win32/builddef.bat
+++ win32/builddef.bat
@@ -1,6 +1,6 @@
+@echo off
 rem Generate phpts.def file, which exports symbols from our dll that
 rem are present in some of the libraries which are compiled statically
 rem into PHP
-rem $Id: builddef.bat,v 1.1 2003/08/22 21:45:58 wez Exp $
-@echo off
+rem $Id: builddef.bat,v 1.2 2003/12/05 17:45:15 wez Exp $
 type ..\ext\sqlite\php_sqlite.def
