75671ed6bbe0193b714ae5b1f8987131cec7e995
andi <andi>
2004-07-19 07:19:50 +0000

    - Nuke empty_string. It is a reminanent from the time where RETURN_FALSE()     used to return "" and not bool(false). It's not worth keeping it     because STR_FREE() and zval_dtor() always have to check for it and it     slows down the general case. In addition, it seems that empty_string     has been abused quite a lot, and was used not only for setting zval's     but generally in PHP code instead of "", which wasn't the intention.     Last but not least, nuking empty_string should improve stability as I     doubt every place correctly checked if they are not mistakenly     erealloc()'ing it or calling efree() on it. NOTE: Some code is probably     broken. Each extension maintainer should check and see that my changes     are OK. Also, I haven't had time to touch PECL yet. Will try and do it     tomorrow.

--- ext/gd/gd_ctx.c
+++ ext/gd/gd_ctx.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: gd_ctx.c,v 1.19 2004/01/08 08:15:29 andi Exp $ */
+/* $Id: gd_ctx.c,v 1.20 2004/01/28 16:25:12 martin Exp $ */
 
 #include "php_gd.h"
 
@@ -82,7 +82,7 @@ static void _php_image_output_ctx(INTERNAL_FUNCTION_PARAMETERS, int image_type, 
 	}
 
 	if ((argc == 2) || (argc > 2 && Z_STRLEN_PP(file))) {
-		if (!fn || fn == empty_string || php_check_open_basedir(fn TSRMLS_CC)) {
+		if (!fn || php_check_open_basedir(fn TSRMLS_CC)) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid filename '%s'", fn);
 			RETURN_FALSE;
 		}
