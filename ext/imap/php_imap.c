f317b18537926503136c9e327a19d101ed81bb96
tony2001 <tony2001>
2005-01-25 14:20:02 +0000

    check body for NULL before using and prevent segfaults

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.201 2005/01/20 18:08:08 tony2001 Exp $ */
+/* $Id: php_imap.c,v 1.202 2005/01/21 10:55:41 tony2001 Exp $ */
 
 #define IMAP41
 
@@ -2653,6 +2653,10 @@ PHP_FUNCTION(imap_bodystruct)
 	object_init(return_value);
 	
 	body=mail_body(imap_le_struct->imap_stream, Z_LVAL_PP(msg), Z_STRVAL_PP(section));
+	if (body == NULL) {
+		zval_dtor(return_value);
+		RETURN_FALSE;
+	}
 	if (body->type <= TYPEMAX) {
 		add_property_long(return_value, "type", body->type);
 	}
