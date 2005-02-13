76c8f8e057f97008d08a032924e99ff0c0182e55
wez <wez>
2005-02-13 21:38:39 +0000

    fix snaps

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -349,7 +349,8 @@ can be built that way. \
 		'debug', 'crt-debug', 'lzf-better-compression',
 		 'php-build', 'snapshot-template',
 		 'pcre-regex', 'fastcgi', 'force-cgi-redirect',
-		 'path-info-check', 'zts', 'ipv6', 'memory-limit'
+		 'path-info-check', 'zts', 'ipv6', 'memory-limit',
+		 'zend-multibyte'
 		);
 	var force;
 
