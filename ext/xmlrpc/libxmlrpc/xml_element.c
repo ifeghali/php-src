f983d778269dd86f19939478340d4b95152b0aa8
jorton <jorton>
2005-04-22 11:06:53 +0000

    Fixed bug #32797 (invalid C code in xmlrpc extension).

--- ext/xmlrpc/libxmlrpc/xml_element.c
+++ ext/xmlrpc/libxmlrpc/xml_element.c
@@ -44,6 +44,9 @@ static const char rcsid[] = "#(@) $Id$";
  *   06/2000
  * HISTORY
  *   $Log$
+ *   Revision 1.9  2005/04/22 11:06:53  jorton
+ *   Fixed bug #32797 (invalid C code in xmlrpc extension).
+ *
  *   Revision 1.8  2005/03/28 00:07:24  edink
  *   Reshufle includes to make it compile on windows
  *
@@ -119,7 +122,7 @@ static const char rcsid[] = "#(@) $Id$";
 #include "queue.h"
 #include "encodings.h"
 
-#define my_free(thing)  if(thing) {free(thing); thing = 0;}
+#define my_free(thing)  if(thing) {free(thing); thing = NULL;}
 
 #define XML_DECL_START                 "<?xml"
 #define XML_DECL_START_LEN             sizeof(XML_DECL_START) - 1
@@ -195,7 +198,10 @@ void xml_elem_free_non_recurse(xml_element* root) {
 
       Q_Destroy(&root->children);
       Q_Destroy(&root->attrs);
-      my_free((char*)root->name);
+      if(root->name) {
+          free((char *)root->name);
+          root->name = NULL;
+      }
       simplestring_free(&root->text);
       my_free(root);
    }
