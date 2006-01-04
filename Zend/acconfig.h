070b4308e5b8f9c162a8b94b5e808da4d1112ad9
andi <andi>
2006-01-04 23:54:12 +0000

    - Update copyright notices to 2006

--- Zend/acconfig.h
+++ Zend/acconfig.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | Zend Engine                                                          |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1998-2005 Zend Technologies Ltd. (http://www.zend.com) |
+   | Copyright (c) 1998-2006 Zend Technologies Ltd. (http://www.zend.com) |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.00 of the Zend license,     |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: acconfig.h,v 1.39 2005/07/29 09:20:32 hyanantha Exp $ */
+/* $Id: acconfig.h,v 1.40 2005/08/03 13:30:45 sniper Exp $ */
 
 #define ZEND_API
 #define ZEND_DLEXPORT
