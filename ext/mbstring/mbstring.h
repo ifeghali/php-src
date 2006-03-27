4f5c26be4460601d28b804f926a8f57bd74ec46d
masugata <masugata>
2006-03-27 15:20:02 +0000

    added mb_list_mime_names( ).

--- ext/mbstring/mbstring.h
+++ ext/mbstring/mbstring.h
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: mbstring.h,v 1.70 2006/03/21 07:47:43 hirokawa Exp $ */
+/* $Id: mbstring.h,v 1.71 2006/03/23 20:14:41 masugata Exp $ */
 
 /*
  * PHP 4 Multibyte String module "mbstring" (currently only for Japanese)
@@ -113,6 +113,7 @@ PHP_FUNCTION(mb_convert_encoding);
 PHP_FUNCTION(mb_detect_encoding);
 PHP_FUNCTION(mb_list_encodings);
 PHP_FUNCTION(mb_list_encodings_alias_names);
+PHP_FUNCTION(mb_list_mime_names);
 PHP_FUNCTION(mb_convert_kana);
 PHP_FUNCTION(mb_encode_mimeheader);
 PHP_FUNCTION(mb_decode_mimeheader);
