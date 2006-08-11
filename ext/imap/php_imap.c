01d828bb228c305b42b0d999b0d854f7a0b9fe54
iliaa <iliaa>
2006-08-11 18:06:28 +0000

    PHP 6 does not have safe_mode

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.218 2006/08/04 20:34:37 iliaa Exp $ */
+/* $Id: php_imap.c,v 1.219 2006/08/11 15:09:43 iliaa Exp $ */
 
 #define IMAP41
 
@@ -825,10 +825,8 @@ PHP_FUNCTION(imap_reopen)
 		imap_le_struct->flags = cl_flags;	
 	}
 
-	/* local filename, need to perform open_basedir and safe_mode checks */
-	if (Z_STRVAL_PP(mailbox)[0] != '{' && 
-			(php_check_open_basedir(Z_STRVAL_PP(mailbox) TSRMLS_CC) || 
-			(PG(safe_mode) && !php_checkuid(Z_STRVAL_PP(mailbox), NULL, CHECKUID_CHECK_FILE_AND_DIR)))) {
+	/* local filename, need to perform open_basedir checks */
+	if (Z_STRVAL_PP(mailbox)[0] != '{' && php_check_open_basedir(Z_STRVAL_PP(mailbox) TSRMLS_CC)) {
 		RETURN_FALSE;
 	}
 
