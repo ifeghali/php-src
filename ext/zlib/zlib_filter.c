1eb3587821af0b4cd85098ec24a6be568af16ba4
cellog <cellog>
2008-01-12 22:28:04 +0000

    wrong file version merged in fix for Bug #40189, here is the correct fix

--- ext/zlib/zlib_filter.c
+++ ext/zlib/zlib_filter.c
@@ -16,7 +16,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: zlib_filter.c,v 1.20 2008/01/12 21:25:43 cellog Exp $ */
+/* $Id: zlib_filter.c,v 1.21 2008/01/12 22:03:44 cellog Exp $ */
 
 #include "php.h"
 #include "php_zlib.h"
@@ -106,7 +106,7 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 			data->strm.avail_in = 0;
 			bin += desired;
 
-			if (status == Z_STREAM_END || data->strm.avail_out < data->outbuf_len) {
+			if (data->strm.avail_out < data->outbuf_len) {
 				php_stream_bucket *out_bucket;
 				size_t bucketlen = data->outbuf_len - data->strm.avail_out;
 				out_bucket = php_stream_bucket_new(stream, estrndup(data->outbuf, bucketlen), bucketlen, 1, 0 TSRMLS_CC);
@@ -114,12 +114,12 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 				data->strm.avail_out = data->outbuf_len;
 				data->strm.next_out = data->outbuf;
 				exit_status = PSFS_PASS_ON;
-				if (status == Z_STREAM_END && data->strm.avail_out >= data->outbuf_len) {
-					/* no more data to decompress, and nothing was spat out */
-					php_stream_bucket_delref(bucket TSRMLS_CC);
-					return PSFS_PASS_ON;
-				}
+			} else if (status == Z_STREAM_END && data->strm.avail_out >= data->outbuf_len) {
+				/* no more data to decompress, and nothing was spat out */
+				php_stream_bucket_delref(bucket TSRMLS_CC);
+				return PSFS_PASS_ON;
 			}
+
 		}
 		consumed += bucket->buflen;
 		php_stream_bucket_delref(bucket TSRMLS_CC);
