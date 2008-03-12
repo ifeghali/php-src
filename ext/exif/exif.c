2b3b71d0cdfb5e9840502e147415cb4d997d9fe0
iliaa <iliaa>
2008-03-12 17:32:26 +0000

    MFB: Fixed bug #44388 (Crash inside exif_read_data() on invalid images)

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id: exif.c,v 1.199 2007/12/31 07:12:09 sebastian Exp $ */
+/* $Id: exif.c,v 1.200 2008/03/07 18:41:59 pajoye Exp $ */
 
 /*  ToDos
  *
@@ -142,7 +142,7 @@ const zend_function_entry exif_functions[] = {
 };
 /* }}} */
 
-#define EXIF_VERSION "1.4 $Id: exif.c,v 1.199 2007/12/31 07:12:09 sebastian Exp $"
+#define EXIF_VERSION "1.4 $Id: exif.c,v 1.200 2008/03/07 18:41:59 pajoye Exp $"
 
 /* {{{ PHP_MINFO_FUNCTION
  */
@@ -2856,7 +2856,7 @@ static int exif_process_IFD_TAG(image_info_type *ImageInfo, char *dir_entry, cha
 					/* exception are IFD pointers */
 					exif_error_docref("exif_read_data#error_ifd" EXIFERR_CC, ImageInfo, E_WARNING, "Process tag(x%04X=%s): Illegal pointer offset(x%04X + x%04X = x%04X > x%04X)", tag, exif_get_tagname(tag, tagname, -12, tag_table TSRMLS_CC), offset_val, byte_count, offset_val+byte_count, IFDlength);
 				}
-				return TRUE;
+				return FALSE;
 			}
 			if (byte_count>sizeof(cbuf)) {
 				/* mark as outside range and get buffer */
