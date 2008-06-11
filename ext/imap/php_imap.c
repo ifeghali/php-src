e20fc039073edc76009f8d8963635cfdbd72614c
philip <philip>
2008-06-11 02:29:39 +0000

    fixed imap_listscan() proto

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -26,7 +26,7 @@
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
-/* $Id: php_imap.c,v 1.250 2008/04/17 11:08:32 felipe Exp $ */
+/* $Id: php_imap.c,v 1.251 2008/05/08 12:00:36 felipe Exp $ */
 
 #define IMAP41
 
@@ -1453,7 +1453,7 @@ PHP_FUNCTION(imap_list_full)
 }
 /* }}} */
 
-/* {{{ proto array imap_scan(resource stream_id, string ref, string pattern, string content)
+/* {{{ proto array imap_listscan(resource stream_id, string ref, string pattern, string content)
    Read list of mailboxes containing a certain string */
 PHP_FUNCTION(imap_listscan)
 {
