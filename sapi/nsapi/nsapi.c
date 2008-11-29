64fa0b8e3c8c4347495449423e105506ec2ddb1d
thetaphi <thetaphi>
2008-11-29 23:43:43 +0000

    support stat cache of webserver (like apache does)

--- sapi/nsapi/nsapi.c
+++ sapi/nsapi/nsapi.c
@@ -17,7 +17,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $Id: nsapi.c,v 1.96 2008/11/29 19:36:40 thetaphi Exp $ */
+/* $Id: nsapi.c,v 1.97 2008/11/29 19:57:23 thetaphi Exp $ */
 
 /*
  * PHP includes
@@ -321,7 +321,7 @@ PHP_MSHUTDOWN_FUNCTION(nsapi)
 PHP_MINFO_FUNCTION(nsapi)
 {
 	php_info_print_table_start();
-	php_info_print_table_row(2, "NSAPI Module Revision", "$Revision: 1.96 $");
+	php_info_print_table_row(2, "NSAPI Module Revision", "$Revision: 1.97 $");
 	php_info_print_table_row(2, "Server Software", system_version());
 	php_info_print_table_row(2, "Sub-requests with nsapi_virtual()",
 	 (nsapi_servact_service)?((zend_ini_long("zlib.output_compression", sizeof("zlib.output_compression"), 0))?"not supported with zlib.output_compression":"enabled"):"not supported on this platform" );
@@ -805,6 +805,13 @@ static int php_nsapi_startup(sapi_module_struct *sapi_module)
 	return SUCCESS;
 }
 
+static struct stat* sapi_nsapi_get_stat(TSRMLS_D)
+{
+	return request_stat_path(
+		SG(request_info).path_translated,
+		((nsapi_request_context *)SG(server_context))->rq
+	);
+}
 
 static sapi_module_struct nsapi_sapi_module = {
 	"nsapi",                                /* name */
@@ -818,7 +825,7 @@ static sapi_module_struct nsapi_sapi_module = {
 
 	sapi_nsapi_ub_write,                    /* unbuffered write */
 	sapi_nsapi_flush,                       /* flush */
-	NULL,                                   /* get uid */
+	sapi_nsapi_get_stat,                    /* get uid/stat */
 	NULL,                                   /* getenv */
 
 	php_error,                              /* error handler */
