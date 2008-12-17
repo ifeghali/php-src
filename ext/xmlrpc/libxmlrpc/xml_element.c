bb98103c85cf3692225d939e3ae0749b7d37f596
iliaa <iliaa>
2008-12-17 00:30:59 +0000

    MFB: removed unused var

--- ext/xmlrpc/libxmlrpc/xml_element.c
+++ ext/xmlrpc/libxmlrpc/xml_element.c
@@ -44,6 +44,10 @@ static const char rcsid[] = "#(@) $Id$";
  *   06/2000
  * HISTORY
  *   $Log$
+ *   Revision 1.13  2008/12/17 00:30:59  iliaa
+ *
+ *   MFB: removed unused var
+ *
  *   Revision 1.12  2008/12/09 17:20:57  iliaa
  *   Comment out fprintf() use inside the library
  *
@@ -722,7 +726,7 @@ xml_element* xml_elem_parse_buf(const char* in_buf, int len, XML_ELEM_INPUT_OPTI
          int line_num = XML_GetCurrentLineNumber(parser);
          int col_num = XML_GetCurrentColumnNumber(parser);
          long byte_idx = XML_GetCurrentByteIndex(parser);
-         int byte_total = XML_GetCurrentByteCount(parser);
+/*         int byte_total = XML_GetCurrentByteCount(parser); */
          const char * error_str = XML_ErrorString(err_code);
          if(byte_idx >= 0) {
              snprintf(buf, 
